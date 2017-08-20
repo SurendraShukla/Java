fe68f577038515a89fc2ec98947cdeb5504933cf
Shukla, Surendra <surenshukla8@gmail.com>
2017-08-20 15:04:08 +0530

    Code refactoring example

---
+++ src/suren/design_patterns/Singleton/EnumSingleton.java
@@ -0,0 +1,11 @@
+package suren.design_patterns.Singleton;
+
+public class EnumSingleton {
+
+//    INSTANCE;
+
+    public static void doSomething(){
+        //do something
+    }
+
+}
---
+++ src/suren/design_patterns/Singleton/ThreadSafeSingleton.java
@@ -0,0 +1,16 @@
+package suren.design_patterns.Singleton;
+
+public class ThreadSafeSingleton {
+
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton(){}
+
+    public static synchronized ThreadSafeSingleton getInstance(){
+        if(instance == null){
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
+
+}
