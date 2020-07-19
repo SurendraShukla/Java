bd4e337b951f19cc990ea1a851ee7e977f23b1b7
Shukla, Surendra <surenshukla8@gmail.com>
2020-07-19 00:16:26 -0700

    LeetCode - findAnagrams, checkInclusion & longestCommonPrefix

--- src/main/me/surendra/leetcode/string/Anagram.java
+++ src/main/me/surendra/leetcode/string/Anagram.java
@@ -1,6 +1,9 @@
 package me.surendra.leetcode.string;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Anagram {
@@ -45,6 +48,67 @@ public class Anagram {
         }
 
         return minStep;
+    }
+
+    public static List<Integer> findAnagrams(String s, String p) {
+        final int smallerStrLen = p.length();
+        final int biggerStrLen = s.length();
+        char[] chars = p.toCharArray();
+        Arrays.sort(chars);
+
+        final List<Integer> subStrIndexes = new ArrayList<>();
+        for(int i = 0; i< biggerStrLen; i++) {
+            int endIndex = i + smallerStrLen;
+            if(endIndex > biggerStrLen) {
+                continue;
+            }
+            String subString = s.substring(i, endIndex);
+            if (isAnagram(subString.toCharArray(), chars)) {
+                subStrIndexes.add(i);
+            }
+        }
+        return subStrIndexes;
+    }
+
+    public static boolean checkInclusion(String s1, String s2) {
+        final int smallerStrLen = s1.length();
+        final int biggerStrLen = s2.length();
+        char[] chars = s1.toCharArray();
+        Arrays.sort(chars);
+
+        for(int i = 0; i< biggerStrLen; i++) {
+            int endIndex = i + smallerStrLen;
+            if(endIndex > biggerStrLen) {
+                continue;
+            }
+            String subString = s2.substring(i, endIndex);
+            if (isAnagram(subString.toCharArray(), chars)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isAnagram(final char[] arr1, final char[] arr2) {
+        Arrays.sort(arr1);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String result = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            int minLength = Math.min(result.length(), strs[i].length());
+            int j = 0;
+            while (j < minLength) {
+                if (result.charAt(j) != strs[i].charAt(j))
+                    break;
+                j++;
+            }
+            result = result.substring(0, j);
+        }
+        return result;
     }
 
 }
