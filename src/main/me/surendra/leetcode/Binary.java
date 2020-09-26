8252cbd6da62e210f5d410c5f5c3f84b0c571124
Surendra Shukla <surenshukla8@gmail.com>
2020-09-26 00:47:49 -0700

    Binary findMaxConsecutiveOnes

---
+++ src/main/me/surendra/leetcode/Binary.java
@@ -0,0 +1,2 @@
+package me.surendra.leetcode;public class Binary {
+}
---
+++ src/test/me/surendra/leetcode/BinaryTest.java
@@ -0,0 +1,7 @@
+package me.surendra.leetcode;
+
+public class Binary {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+    }
+}
