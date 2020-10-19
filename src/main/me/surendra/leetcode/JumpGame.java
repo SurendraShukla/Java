a65fcbc1506714b5c673e8fc8562b1350488a359
Surendra Shukla <surenshukla8@gmail.com>
2020-10-18 23:49:50 -0700

    LeetCode - Jump Game Program

---
+++ src/main/me/surendra/leetcode/JumpGame.java
@@ -0,0 +1,15 @@
+package me.surendra.leetcode;
+
+public class JumpGame {
+
+    public static boolean canJump(int[] nums) {
+        int lastGoodIndexPosition = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastGoodIndexPosition) {
+                lastGoodIndexPosition = i;
+            }
+        }
+        return lastGoodIndexPosition == 0;
+    }
+
+}
