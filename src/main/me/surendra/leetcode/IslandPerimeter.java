816a5a3abe3ada57e1ae9c3f7a52cdff1a259c03
Surendra Shukla <surenshukla8@gmail.com>
2021-04-25 19:19:33 -0700

    IsLand Perimeter finder by 2 ways

---
+++ src/main/me/surendra/leetcode/IslandPerimeter.java
@@ -0,0 +1,66 @@
+package me.surendra.leetcode;
+
+/**
+ * @see <a href="https://leetcode.com/problems/island-perimeter/">Island Perimeter</a>
+ */
+public class IslandPerimeter {
+
+    public static int getLandPerimeterByCheckingAtAllFourSides(final int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int perimeter = 0;
+
+        for (int row = 0; row<rows; row++) {
+            for (int col = 0; col<cols; col++) {
+
+                if(grid[row][col] == 1) {
+                    perimeter += 4;
+                    // Top
+                    if ((row > 0) && (grid[row - 1][col] == 1)) {
+                        perimeter -= 1;
+                    }
+                    // Right
+                    if ((col < cols - 1) && (grid[row][col + 1] == 1)) {
+                        perimeter -= 1;
+                    }
+                    // Bottom
+                    if ((row < rows - 1) && (grid[row + 1][col] == 1)) {
+                        perimeter -= 1;
+                    }
+                    // Left
+                    if ((col > 0) && (grid[row][col - 1] == 1)) {
+                        perimeter -= 1;
+                    }
+                }
+
+            }
+        }
+
+        return perimeter;
+    }
+
+    public static int getLandPerimeterByCheckingUpAndLeftSide(final int[][] grid) {
+        int perimeter = 0;
+
+        for (int row=0; row<grid.length; row++) {
+            for (int col=0; col<grid[0].length; col++) {
+
+                if (grid[row][col] == 1) {
+                    perimeter += 4;
+                    // Left
+                    if ((col > 0) && (grid[row][col - 1] == 1)) {
+                        perimeter -= 2;
+                    }
+                    // Top
+                    if ((row > 0) && (grid[row - 1][col] == 1)) {
+                        perimeter -= 2;
+                    }
+                }
+
+            }
+        }
+
+        return perimeter;
+    }
+
+}
---
+++ src/test/me/surendra/leetcode/IslandPerimeterTest.java
@@ -0,0 +1,39 @@
+package me.surendra.leetcode;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static me.surendra.leetcode.IslandPerimeter.getLandPerimeterByCheckingAtAllFourSides;
+import static me.surendra.leetcode.IslandPerimeter.getLandPerimeterByCheckingUpAndLeftSide;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class IslandPerimeterTest extends TestCase {
+
+    @Test
+    public void test1() {
+        int[][] grid = {
+            {0, 1, 0, 0},
+            {1, 1, 1, 0},
+            {0, 1, 0, 0},
+            {1, 1, 0, 0}
+        };
+        assertThat(getLandPerimeterByCheckingAtAllFourSides(grid), equalTo(16));
+        assertThat(getLandPerimeterByCheckingUpAndLeftSide(grid), equalTo(16));
+    }
+
+    @Test
+    public void test2() {
+        int[][] grid = {{1}};
+        assertThat(getLandPerimeterByCheckingAtAllFourSides(grid), equalTo(4));
+        assertThat(getLandPerimeterByCheckingUpAndLeftSide(grid), equalTo(4));
+    }
+
+    @Test
+    public void test3() {
+        int[][] grid = {{1,0}};
+        assertThat(getLandPerimeterByCheckingAtAllFourSides(grid), equalTo(4));
+        assertThat(getLandPerimeterByCheckingUpAndLeftSide(grid), equalTo(4));
+    }
+
+}
