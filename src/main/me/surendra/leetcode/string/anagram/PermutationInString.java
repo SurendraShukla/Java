d89283dcd57315b01e4c475e6a93e49e870e8b3f
Surendra Shukla <surenshukla8@gmail.com>
2021-05-03 02:20:55 -0700

    Search string permutation in a string using charArray

--- src/main/me/surendra/leetcode/string/anagram/PermutationInString.java
+++ src/main/me/surendra/leetcode/string/anagram/PermutationInString.java
@@ -2,9 +2,82 @@ package me.surendra.leetcode.string.anagram;
 
 import java.util.Arrays;
 
+/**
+ * @see <a href="https://leetcode.com/problems/permutation-in-string/">Permutation in String</a>
+ */
 public class PermutationInString {
 
-    public boolean checkInclusion(String searchStr, String mainString) {
+    /*
+        Time Complexity - O(l1 + 26 * l1 * (l2-l1) ). l1 is the length of string l1, l1 is the length of string l2
+        Space Complexity - O(1). s1map and s2map of size 26 is used.
+     */
+    public boolean checkInclusionByUsingIntArray(String searchStr, String mainStr) {
+        final int[] searchCharArray = new int[26];
+        final int searchStrLength = searchStr.length();
+        final int mainStrLength = mainStr.length();
+
+        for (int i = 0; i < searchStrLength; i++) {
+            searchCharArray[searchStr.charAt(i) - 'a']++;
+        }
+
+        for (int j = 0; j <= mainStrLength - searchStrLength; j++) {
+            final int[] tmpCharArray = new int[26];
+            for (int k = 0; k < searchStrLength; k++) {
+                tmpCharArray[mainStr.charAt(j+k) - 'a']++;
+            }
+            if (charArrayIsSame(searchCharArray, tmpCharArray)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean charArrayIsSame(final int[] searchCharArray, final int[] tmpCharArray) {
+        for(int i=0; i<searchCharArray.length; i++) {
+            if (searchCharArray[i] != tmpCharArray[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+        Time Complexity - O( l1log(l1) + (l2-l1)l1log(l1) ). l1 is the length of string l1, l1 is the length of string l2
+        Space Complexity - O(l1)
+     */
+    public boolean checkInclusionByUsingString(String searchStr, String mainStr) {
+        final int searchStrLength = searchStr.length();
+        final int mainStrLength = mainStr.length();
+
+        final char[] sortedSearchStr = sortString(searchStr);
+
+        for (int i = 0; i <= mainStrLength-searchStrLength; i++) {
+            final char[] sortedSubString = sortString(mainStr.substring(i, i+searchStrLength));
+            if(Arrays.equals(sortedSearchStr, sortedSubString)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Method to sort a string alphabetically
+    public static char[] sortString(String inputString) {
+
+        // convert input string to char array
+        char tempArray[] = inputString.toCharArray();
+
+        // sort tempArray
+        Arrays.sort(tempArray);
+
+        // return new sorted string
+        return tempArray;
+    }
+
+    /*
+        Time Complexity - O( l1log(l1) + (l2-l1)l1log(l1) ). l1 is the length of string l1, l1 is the length of string l2
+        Space Complexity - O(l1)
+     */
+    public boolean checkInclusionByUsingCharArray(String searchStr, String mainString) {
         final char[] searchChars = searchStr.toCharArray();
         final char[] mainChars = mainString.toCharArray();
         final int searchLength = searchChars.length;
--- src/test/me/surendra/leetcode/string/anagram/PermutationInStringTest.java
+++ src/test/me/surendra/leetcode/string/anagram/PermutationInStringTest.java
@@ -1,9 +1,10 @@
 package me.surendra.leetcode.string.anagram;
 
+
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class PermutationInStringTest {
@@ -11,20 +12,44 @@ public class PermutationInStringTest {
     PermutationInString permutationInString;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         permutationInString = new PermutationInString();
     }
 
     @Test
-    public void strContainsPermutation() {
-        assertThat(permutationInString.checkInclusion("ab", "eidbaooo"), equalTo(true));
-        assertThat(permutationInString.checkInclusion("adc", "dcda"), equalTo(true));
+    public void strContainsPermutationForCheckInclusionByUsingIntArray() {
+        assertThat(permutationInString.checkInclusionByUsingIntArray("ab", "eidbaooo"), equalTo(true));
+        assertThat(permutationInString.checkInclusionByUsingIntArray("adc", "dcda"), equalTo(true));
     }
 
     @Test
-    public void strDoesNotContainsPermutation() {
-        assertThat(permutationInString.checkInclusion("ab", "eidboaoo"), equalTo(false));
+    public void strDoesNotContainsPermutationForCheckInclusionByUsingIntArray() {
+        assertThat(permutationInString.checkInclusionByUsingIntArray("ab", "eidboaoo"), equalTo(false));
+        assertThat(permutationInString.checkInclusionByUsingIntArray("ab", "eidboaoo"), equalTo(false));
     }
 
+    @Test
+    public void strContainsPermutationForCheckInclusionByUsingString() {
+        assertThat(permutationInString.checkInclusionByUsingString("ab", "eidbaooo"), equalTo(true));
+        assertThat(permutationInString.checkInclusionByUsingString("adc", "dcda"), equalTo(true));
+    }
+
+    @Test
+    public void strDoesNotContainsPermutationForCheckInclusionByUsingString() {
+        assertThat(permutationInString.checkInclusionByUsingString("ab", "eidboaoo"), equalTo(false));
+        assertThat(permutationInString.checkInclusionByUsingString("ab", "eidboaoo"), equalTo(false));
+    }
+
+    @Test
+    public void strContainsPermutationForCheckInclusionByUsingCharArray() {
+        assertThat(permutationInString.checkInclusionByUsingCharArray("ab", "eidbaooo"), equalTo(true));
+        assertThat(permutationInString.checkInclusionByUsingCharArray("adc", "dcda"), equalTo(true));
+    }
+
+    @Test
+    public void strDoesNotContainsPermutationForCheckInclusionByUsingCharArray() {
+        assertThat(permutationInString.checkInclusionByUsingCharArray("ab", "eidboaoo"), equalTo(false));
+        assertThat(permutationInString.checkInclusionByUsingCharArray("ab", "eidboaoo"), equalTo(false));
+    }
 
 }
