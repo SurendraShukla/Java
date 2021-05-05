f63d060f0df6a397d7b01135c5b02a55bdc8b8be
Surendra Shukla <surenshukla8@gmail.com>
2021-05-05 02:01:30 -0700

    Leet Code - Cyclic Linked list Checker

---
+++ src/main/me/surendra/leetcode/linked_list/LinkedListNodeChecker.java
@@ -0,0 +1,48 @@
+package me.surendra.leetcode.linked_list;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @see <a href="https://leetcode.com/problems/linked-list-cycle">Linked List Cycle</a>
+ */
+public class LinkedListNodeChecker {
+
+    /*
+        Time Complexity - O(n)
+        Space Complexity - O(n)
+     */
+    public static boolean hasCycle(ListNode head) {
+        final Set<ListNode> nodesSeen = new HashSet<>();
+        while(head != null) {
+            if (nodesSeen.contains(head)) {
+                return true;
+            }
+            nodesSeen.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+
+    /*
+        Time Complexity - O(n)
+        Space Complexity - O(1)
+     */
+    public static boolean hasCycleUsing2Runner(ListNode head) {
+        if(head == null || head.next == null)
+            return false;
+
+        ListNode slowRunnerNode = head;
+        ListNode fastRunnerNode = head.next;
+
+        while(slowRunnerNode != fastRunnerNode) {
+            if (fastRunnerNode == null || fastRunnerNode.next == null) {
+                return false;
+            }
+            slowRunnerNode = head.next;
+            fastRunnerNode = head.next.next;
+        }
+        return true;
+    }
+
+}
