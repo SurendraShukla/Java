7514358a346e3310a00199363d6f462d8a4e7aff
Surendra Shukla <surenshukla8@gmail.com>
2021-10-04 21:48:48 -0700

    Leet Code - Dynamic Programming Best Time to Buy and Sell Stock

---
+++ src/main/me/surendra/leetcode/dynamic_programming/StockProfitBooker.java
@@ -0,0 +1,23 @@
+package me.surendra.leetcode.dynamic_programming;
+
+/**
+ * @see <a href="https://leetcode.com/problems/best-time-to-buy-and-sell-stock/">Best Time to Buy and Sell Stock</a>
+ */
+public class StockProfitBooker {
+
+    /*
+        Time Complexity - O(n)
+        Space Complexity - O(1)
+     */
+    public int maxProfit(final int[] prices) {
+        final int length = prices.length;
+        int maxProfit = 0;
+        int minVal = prices[0];
+        for(int i = 1; i< length; i++) {
+            minVal = Math.min(prices[i], minVal);
+            maxProfit = Math.max(prices[i] - minVal, maxProfit);
+        }
+        return maxProfit;
+    }
+
+}
