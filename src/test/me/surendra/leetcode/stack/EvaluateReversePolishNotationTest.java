0ffa0b04bb0dcd05292eb56de1bc5a1a9288507e
Surendra Shukla <surenshukla8@gmail.com>
2022-07-23 13:02:02 -0700

    LeetCode; Stack - Evaluate Reverse Polish Notation

--- src/test/me/surendra/leetcode/stack/EvaluateReversePolishNotationTest.java
+++ src/test/me/surendra/leetcode/stack/EvaluateReversePolishNotationTest.java
@@ -11,15 +11,16 @@ public class EvaluateReversePolishNotationTest {
 
     private int callMethod(final String[] tokens) {
         return evaluateReversePolishNotation.evalRPN(tokens);
+//        return evaluateReversePolishNotation.evalRPNUsingHashMap(tokens);
     }
 
     @Test
     public void test() {
         assertThat(callMethod(new String[]{"5", "3", "+"}), equalTo(8));
-        assertThat(callMethod(new String[]{"2","1","+","3","*"}), equalTo(9));
-        assertThat(callMethod(new String[]{"4","13","5","/","+"}), equalTo(6));
-        assertThat(callMethod(new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"}), equalTo(22));
-        assertThat(callMethod(new String[]{"15", "7", "1", "1", "+", "-", "/", "3", "*", "2", "1", "1", "+", "+", "-"}), equalTo(5));
+        assertThat(callMethod(new String[]{"2", "1", "+", "3", "*"}), equalTo(9));
+        assertThat(callMethod(new String[]{"4", "13", "5", "/", "+"}), equalTo(6));
+        assertThat(callMethod(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}), equalTo(22));
+        assertThat(callMethod(new String[]{"15",  "7",  "1",  "1",  "+",  "-",  "/",  "3",  "*",  "2",  "1",  "1",  "+",  "+",  "-"}), equalTo(5));
     }
 
 }
