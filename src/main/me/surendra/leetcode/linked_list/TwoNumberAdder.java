cfd7a4340201dbe4fcc79ab143f3a8084b49fc5e
Surendra Shukla <surenshukla8@gmail.com>
2022-11-21 01:12:24 -0800

    LeetCode; Linked List - Add Two Numbers

--- src/main/me/surendra/leetcode/linked_list/TwoNumberAdder.java
+++ src/main/me/surendra/leetcode/linked_list/TwoNumberAdder.java
@@ -1,23 +1,25 @@
 package me.surendra.leetcode.linked_list;
 
+
 /**
  * @see <a href="https://leetcode.com/problems/add-two-numbers/">Add Two Numbers</a>
  */
 public class TwoNumberAdder {
 
     /*
-        Time complexity: O(m*n) m and n represents the length of l1 and l2
-        Space complexity: O(m*n)
+        Time complexity: O(max(m, n)) m and n represents the length of l1 and l2
+        Space complexity: O(max(m, n))
 
         Dummy node simplifies the code. Without a dummy head, need to write extra conditional statements to initialize the head's value.
      */
     public ListNode addTwoNumbersUsingDummyNode(ListNode l1, ListNode l2) {
+        final ListNode returnNode = new ListNode(0);
+        ListNode dummyNode = returnNode;
         int carry = 0;
-        final ListNode dummyNode = new ListNode(0);
-        ListNode currentNode = dummyNode;
 
         while (l1 != null || l2 != null) {
-            int l1NodeVal = 0, l2NodeVal = 0;
+            int l1NodeVal = 0;
+            int l2NodeVal = 0;
             if (l1 != null) {
                 l1NodeVal = l1.val;
                 l1 = l1.next;
@@ -26,70 +28,16 @@ public class TwoNumberAdder {
                 l2NodeVal = l2.val;
                 l2 = l2.next;
             }
-            int val = carry + l1NodeVal + l2NodeVal;
-            carry = (val > 9) ? 1 : 0;
-            currentNode.next = new ListNode(val%10);
-            currentNode = currentNode.next;
+            final int sum = carry + l1NodeVal + l2NodeVal;
+            carry = sum / 10;
+            dummyNode.next = new ListNode(sum % 10);
+            dummyNode = dummyNode.next;
         }
 
         if (carry > 0) {
-            currentNode.next = new ListNode(carry);
+            dummyNode.next = new ListNode(carry);
         }
-        return dummyNode.next;
-    }
-
-    private int carry = 0;
-    ListNode returnListNode = null;
-
-    /*
-        Time complexity: O(m*n) m and n represents the length of l1 and l2
-        Space complexity: O(m*n)
-     */
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        while (l1 != null || l2 != null) {
-            int l1Val = 0, l2Val = 0;
-            if (l1 != null) {
-                l1Val = l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                l2Val = l2.val;
-                l2 = l2.next;
-            }
-            populateListNode(carry + l1Val + l2Val);
-        }
-
-        if (carry > 0) {
-            populateListNode(carry);
-        }
-        return returnListNode;
-    }
-
-    private void populateListNode(final int tmpVal) {
-        int val;
-        if (tmpVal < 10) {
-            val = tmpVal;
-            carry = 0;
-        } else {
-            val = tmpVal % 10;
-            carry = 1;
-        }
-        populateNode(val);
-    }
-
-    private void populateNode(final int val) {
-        final ListNode newNode = new ListNode(val);
-        if (returnListNode == null) {
-            returnListNode = newNode;
-        }else if (returnListNode.next == null) {
-            returnListNode.next = newNode;
-        }else{
-            ListNode tmpListNode = returnListNode;
-            while(tmpListNode.next != null) {
-                tmpListNode = tmpListNode.next;
-            }
-            tmpListNode.next = newNode;
-        }
+        return returnNode.next;
     }
 
 }
--- src/test/me/surendra/leetcode/linked_list/TwoNumberAdderTest.java
+++ src/test/me/surendra/leetcode/linked_list/TwoNumberAdderTest.java
@@ -1,15 +1,16 @@
 package me.surendra.leetcode.linked_list;
 
-import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 
 public class TwoNumberAdderTest {
 
-    private TwoNumberAdder twoNumberAdder;
-
-    @Before
-    public void setUp() throws Exception {
-        twoNumberAdder = new TwoNumberAdder();
+    private TwoNumberAdder twoNumberAdder = new TwoNumberAdder();
+    public ListNode callMethod(final ListNode listNode1, final ListNode listNode2) {
+        return twoNumberAdder.addTwoNumbersUsingDummyNode(listNode1, listNode2);
     }
 
     @Test
@@ -17,8 +18,10 @@ public class TwoNumberAdderTest {
         final ListNode listNode1 = LinkedList.from(new int[]{2, 4, 3});
         final ListNode listNode2 = LinkedList.from(new int[]{5, 6, 4});
 
-//        expected 7->0->8
-        twoNumberAdder.addTwoNumbersUsingDummyNode(listNode1, listNode2);
+        final ListNode actual = callMethod(listNode1, listNode2);
+
+        final ListNode expected = LinkedList.from(new int[]{7, 0, 8});
+        assertThat(actual, equalTo(expected));
     }
 
     @Test
@@ -26,17 +29,21 @@ public class TwoNumberAdderTest {
         final ListNode listNode1 = LinkedList.from(new int[]{0});
         final ListNode listNode2 = LinkedList.from(new int[]{0});
 
-//        expected 0
-        twoNumberAdder.addTwoNumbersUsingDummyNode(listNode1, listNode2);
+        final ListNode actual = callMethod(listNode1, listNode2);
+
+        final ListNode expected = LinkedList.from(new int[]{0});
+        assertThat(actual, equalTo(expected));
     }
 
     @Test
     public void test3() {
-        final ListNode listNode1 = LinkedList.from(new int[]{9,9,9,9,9,9,9});
-        final ListNode listNode2 = LinkedList.from(new int[]{9,9,9,9});
+        final ListNode listNode1 = LinkedList.from(new int[]{9, 9, 9, 9, 9, 9, 9});
+        final ListNode listNode2 = LinkedList.from(new int[]{9, 9, 9, 9});
 
-//        expected 8->9->9->9->0->0->0->1
-        twoNumberAdder.addTwoNumbersUsingDummyNode(listNode1, listNode2);
+        final ListNode actual = callMethod(listNode1, listNode2);
+
+        final ListNode expected = LinkedList.from(new int[]{8, 9, 9, 9, 0, 0, 0, 1});
+        assertThat(actual, equalTo(expected));
     }
 
 }
