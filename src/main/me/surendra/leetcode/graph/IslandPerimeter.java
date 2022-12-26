489cefae8d77089810e3355e345862854960f403
Surendra Shukla <surenshukla8@gmail.com>
2022-12-25 23:49:00 -0800

    LeetCode; Graph - Island Perimeter

--- src/main/me/surendra/leetcode/graph/IslandPerimeter.java
+++ src/main/me/surendra/leetcode/graph/IslandPerimeter.java
@@ -6,37 +6,38 @@ package me.surendra.leetcode.graph;
 public class IslandPerimeter {
     
     /*
-        Time complexity: O(m*n)
+        Time complexity: O(m * n)
         Space complexity: O(1)
      */
-    public static int getLandPerimeterByCheckingAtAllFourSides(final int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
+    int rowMax;
+    int colMax;
+    int[][] matrix;
+    public int getLandPerimeterByCheckingAtAllFourSides(final int[][] grid) {
+        rowMax = grid.length;
+        colMax = grid[0].length;
+
+        final int[] rows = {0, 1, 0, -1};
+        final int[] cols = {1, 0, -1, 0};
+
         int perimeter = 0;
-
-        for (int row = 0; row<rows; row++) {
-            for (int col = 0; col<cols; col++) {
-
-                if(grid[row][col] == 1) {
-                    perimeter += 4;
-                    // Top
-                    if ((row > 0) && (grid[row - 1][col] == 1)) {
-                        perimeter -= 1;
-                    }
-                    // Right
-                    if ((col < cols - 1) && (grid[row][col + 1] == 1)) {
-                        perimeter -= 1;
-                    }
-                    // Bottom
-                    if ((row < rows - 1) && (grid[row + 1][col] == 1)) {
-                        perimeter -= 1;
-                    }
-                    // Left
-                    if ((col > 0) && (grid[row][col - 1] == 1)) {
-                        perimeter -= 1;
-                    }
+        for (int row = 0; row < rowMax; row++) {
+            for (int col = 0; col < colMax; col++) {
+                if (grid[row][col] == 0) {
+                    continue;
                 }
+                perimeter += 4;
 
+                for (int dIdx = 0; dIdx < rows.length; dIdx++) {
+                    final int nextRow = rows[dIdx] + row;
+                    final int nextCol = cols[dIdx] + col;
+                    if (nextRow < 0 || nextRow >= rowMax ||
+                        nextCol < 0 || nextCol >= colMax ||
+                        grid[nextRow][nextCol] == 0
+                    ) {
+                        continue;
+                    }
+                    perimeter -= 1;
+                }
             }
         }
 
@@ -44,15 +45,14 @@ public class IslandPerimeter {
     }
 
     /*
-    Time complexity: O(m*n)
-    Space complexity: O(1)
+        Time complexity: O(m * n)
+        Space complexity: O(1)
      */
-    public static int getLandPerimeterByCheckingUpAndLeftSide(final int[][] grid) {
+    public int getLandPerimeterByCheckingUpAndLeftSide(final int[][] grid) {
         int perimeter = 0;
 
-        for (int row=0; row<grid.length; row++) {
-            for (int col=0; col<grid[0].length; col++) {
-
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
                 if (grid[row][col] == 1) {
                     perimeter += 4;
                     // Left
@@ -64,10 +64,45 @@ public class IslandPerimeter {
                         perimeter -= 2;
                     }
                 }
-
             }
         }
 
+        return perimeter;
+    }
+
+    /*
+        Time complexity: O(m * n)
+        Space complexity: O(1)
+     */
+    public int islandPerimeterBfs(final int[][] grid) {
+        rowMax = grid.length;
+        colMax = grid[0].length;
+        matrix = grid;
+
+        for (int row = 0; row < rowMax; row++) {
+            for (int col = 0; col < colMax; col++) {
+                if (matrix[row][col] == 1) {
+                    return islandPerimeterUsingBfs(row, col);
+                }
+            }
+        }
+        return 0;
+    }
+
+    private int islandPerimeterUsingBfs(final int row, final int col) {
+        if (row < 0 || row >= rowMax || col < 0 || col >= colMax || matrix[row][col] == 0) {
+            return 1;
+        }
+        if (matrix[row][col] == 2) {
+            return 0;
+        }
+        matrix[row][col] = 2;
+
+        int perimeter = 0;
+        perimeter += islandPerimeterUsingBfs(row, col - 1);
+        perimeter += islandPerimeterUsingBfs(row, col + 1);
+        perimeter += islandPerimeterUsingBfs(row - 1, col);
+        perimeter += islandPerimeterUsingBfs(row + 1, col);
         return perimeter;
     }
 
--- src/test/me/surendra/leetcode/graph/IslandPerimeterTest.java
+++ src/test/me/surendra/leetcode/graph/IslandPerimeterTest.java
@@ -1,39 +1,35 @@
 package me.surendra.leetcode.graph;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
-import static me.surendra.leetcode.graph.IslandPerimeter.getLandPerimeterByCheckingAtAllFourSides;
-import static me.surendra.leetcode.graph.IslandPerimeter.getLandPerimeterByCheckingUpAndLeftSide;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-public class IslandPerimeterTest extends TestCase {
+public class IslandPerimeterTest {
+
+    final IslandPerimeter islandPerimeter = new IslandPerimeter();
+
+    private int callMethod(final int[][] grid) {
+        return islandPerimeter.islandPerimeterBfs(grid);
+//        return islandPerimeter.getLandPerimeterByCheckingUpAndLeftSide(grid);
+//        return islandPerimeter.getLandPerimeterByCheckingAtAllFourSides(grid);
+    }
 
     @Test
     public void test1() {
-        int[][] grid = {
+        final int[][] grid = {
             {0, 1, 0, 0},
             {1, 1, 1, 0},
             {0, 1, 0, 0},
             {1, 1, 0, 0}
         };
-        assertThat(getLandPerimeterByCheckingAtAllFourSides(grid), equalTo(16));
-        assertThat(getLandPerimeterByCheckingUpAndLeftSide(grid), equalTo(16));
+        assertThat(callMethod(grid), equalTo(16));
     }
 
     @Test
     public void test2() {
-        int[][] grid = {{1}};
-        assertThat(getLandPerimeterByCheckingAtAllFourSides(grid), equalTo(4));
-        assertThat(getLandPerimeterByCheckingUpAndLeftSide(grid), equalTo(4));
-    }
-
-    @Test
-    public void test3() {
-        int[][] grid = {{1,0}};
-        assertThat(getLandPerimeterByCheckingAtAllFourSides(grid), equalTo(4));
-        assertThat(getLandPerimeterByCheckingUpAndLeftSide(grid), equalTo(4));
+        assertThat(callMethod(new int[][]{{1}}), equalTo(4));
+        assertThat(callMethod(new int[][]{{1, 0}}), equalTo(4));
     }
 
 }
