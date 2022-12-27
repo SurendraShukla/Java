8876fb12d2d62c5cc1f974787cd87557726b76d0
Surendra Shukla <surenshukla8@gmail.com>
2022-12-27 00:38:34 -0800

    LeetCode; Binary Search - Search a 2D Matrix

---
+++ src/main/me/surendra/leetcode/search/binary_search/SearchIn2DMatrix.java
@@ -0,0 +1,34 @@
+package me.surendra.leetcode.search.binary_search;
+
+
+/**
+ * @see <a href="https://leetcode.com/problems/search-a-2d-matrix/">Search a 2D Matrix</a>
+ */
+public class SearchIn2DMatrix {
+
+    public boolean searchMatrix(final int[][] matrix, final int target) {
+        final int rowMax = matrix.length;
+        final int colMax = matrix[0].length;
+
+        int left = 0;
+        int right = rowMax * colMax - 1;
+
+        while (left <= right) {
+            final int mid = left + ((right - left) / 2);
+            // idx = row * colMax + col
+            final int row = mid / colMax;
+            final int col = mid % colMax;
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            if (matrix[row][col] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+}
---
+++ src/test/me/surendra/leetcode/search/binary_search/SearchIn2DMatrixTest.java
@@ -0,0 +1,25 @@
+package me.surendra.leetcode.search.binary_search;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+
+public class SearchIn2DMatrixTest {
+
+    private final SearchIn2DMatrix searchIn2DMatrix = new SearchIn2DMatrix();
+
+    private boolean callMethod(final int[][] matrix, final int target) {
+        return searchIn2DMatrix.searchMatrix(matrix, target);
+    }
+
+    @Test
+    public void test() {
+        final int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+
+        assertThat(callMethod(matrix, 3), equalTo(true));
+        assertThat(callMethod(matrix, 13), equalTo(false));
+    }
+
+}
