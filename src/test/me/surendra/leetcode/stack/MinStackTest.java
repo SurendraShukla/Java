6fb727952c24cede40bd32d738c6c34df7699ad6
Surendra Shukla <surenshukla8@gmail.com>
2023-01-03 01:23:44 -0800

    LeetCode; Stack - MinStack

--- src/test/me/surendra/leetcode/stack/MinStackTest.java
+++ src/test/me/surendra/leetcode/stack/MinStackTest.java
@@ -8,38 +8,38 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class MinStackTest {
-    private MinStack minStackImpl;
+    private MinStack minStack;
 
     @Before
     public void setUp() throws Exception {
-        minStackImpl = new MinStackImplWith2Stacks();
+        minStack = new MinStackImplWith2Stacks();
 //        minStackImpl = new MinStackImplWithOneStack();
     }
 
     @Test
     public void test1() {
-        minStackImpl.push(-2);
-        minStackImpl.push(0);
-        minStackImpl.push(-3);
-        assertThat(minStackImpl.getMin(), equalTo(-3));
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        assertThat(minStack.getMin(), equalTo(-3));
 
-        minStackImpl.pop();
-        assertThat(minStackImpl.top(), equalTo(0));
-        assertThat(minStackImpl.getMin(), equalTo(-2));
+        minStack.pop();
+        assertThat(minStack.top(), equalTo(0));
+        assertThat(minStack.getMin(), equalTo(-2));
     }
 
 
     @Test
     public void test2() {
-        minStackImpl.push(-2);
-        minStackImpl.push(0);
-        minStackImpl.push(-1);
-        assertThat(minStackImpl.getMin(), equalTo(-2));
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-1);
+        assertThat(minStack.getMin(), equalTo(-2));
 
-        assertThat(minStackImpl.top(), equalTo(-1));
-        minStackImpl.pop();
+        assertThat(minStack.top(), equalTo(-1));
+        minStack.pop();
 
-        assertThat(minStackImpl.getMin(), equalTo(-2));
+        assertThat(minStack.getMin(), equalTo(-2));
     }
 
 }
