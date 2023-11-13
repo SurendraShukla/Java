d970b01e434ef58f61f912158c9fa62bebbba4c7
Surendra Shukla <surenshukla8@gmail.com>
2023-11-13 14:24:06 -0800

    LeetCode Binary Search - Check If a Number Is Majority Element in a Sorted Array

---
+++ learning/src/main/me/surendra/leetcode/search/binary_search/MajorityElementSearchInASortedArray.java
@@ -0,0 +1,52 @@
+package me.surendra.leetcode.search.binary_search;
+
+/**
+ * @see <a href="https://leetcode.com/problems/check-if-a-number-is-majority-element-in-a-sorted-array/">Check If a Number Is Majority Element in a Sorted Array</a>
+ */
+public class MajorityElementSearchInASortedArray {
+
+    /*
+        Time Complexity - O(n log n)
+        Space Complexity - 0(n)
+     */
+    public boolean isMajorityElement(final int[] nums, final int target) {
+        final Integer leftIdx = binarySearch(nums, target, true);
+        if (leftIdx == null) {
+            return false;
+        }
+        final Integer rightIdx = binarySearch(nums, target, false);
+        return (rightIdx - leftIdx) + 1 > nums.length / 2;
+    }
+
+    private Integer binarySearch(final int[] nums, final int target, final boolean firstIdx) {
+        int left = 0;
+        int right = nums.length;
+        int mid = (left + right) / 2;
+        Integer idx = null;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (mid < 0 || mid >= nums.length) {
+                return idx;
+            }
+            if (nums[mid] == target) {
+                if (idx == null) {
+                    idx = mid;
+                }
+                if (firstIdx) {
+                    idx = Math.min(idx, mid);
+                    right = mid - 1;
+                } else {
+                    idx = Math.max(idx, mid);
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return idx;
+    }
+
+}
---
+++ learning/src/test/me/surendra/leetcode/search/binary_search/MajorityElementSearchInASortedArrayTest.java
@@ -0,0 +1,29 @@
+package me.surendra.leetcode.search.binary_search;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class MajorityElementSearchInASortedArrayTest {
+
+    final MajorityElementSearchInASortedArray majorityElementSearchInASortedArray = new MajorityElementSearchInASortedArray();
+
+    private boolean callMethod(final int[] nums, final int target) {
+        return majorityElementSearchInASortedArray.isMajorityElement(nums, target);
+    }
+
+    @Test
+    public void name() {
+        assertThat(callMethod(new int[]{1}, 1), equalTo(true));
+        assertThat(callMethod(new int[]{1, 2, 2}, 2), equalTo(true));
+        assertThat(callMethod(new int[]{438885258, 438885258}, 438885258), equalTo(true));
+        assertThat(callMethod(new int[]{2, 4, 5, 5, 5, 5, 5, 6, 6}, 5), equalTo(true));
+
+        assertThat(callMethod(new int[]{1}, 2), equalTo(false));
+        assertThat(callMethod(new int[]{1, 2, 3, 4, 5}, 3), equalTo(false));
+        assertThat(callMethod(new int[]{10, 100, 101, 101}, 101), equalTo(false));
+    }
+
+}
