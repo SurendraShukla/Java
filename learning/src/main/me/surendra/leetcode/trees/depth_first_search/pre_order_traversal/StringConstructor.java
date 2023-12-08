370dbd303dae7df4ac3c3f8f2697a280c22c4aac
Surendra Shukla <surenshukla8@gmail.com>
2023-12-07 23:31:14 -0800

    LeetCode Tree - Construct String from Binary Tree

--- learning/src/main/me/surendra/leetcode/trees/depth_first_search/pre_order_traversal/StringConstructor.java
+++ learning/src/main/me/surendra/leetcode/trees/depth_first_search/pre_order_traversal/StringConstructor.java
@@ -3,6 +3,7 @@ package me.surendra.leetcode.trees.depth_first_search.pre_order_traversal;
 
 import me.surendra.leetcode.trees.TreeNode;
 
+
 /**
  * @see <a href="https://leetcode.com/problems/construct-string-from-binary-tree/">Construct String from Binary Tree</a>
  */
@@ -12,53 +13,32 @@ public class StringConstructor {
         Time complexity: O(n)
         Space complexity: O(n)
      */
-    final StringBuilder stringBuilder = new StringBuilder();
-    public String tree2strUsingHelper(final TreeNode root) {
-        populateString(root);
-        return stringBuilder.toString();
-    }
-
-    private void populateString(final TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        stringBuilder.append(root.val);
-        if (root.left == null && root.right == null) {
-            return;
-        }
-        stringBuilder.append("(");
-        populateString(root.left);
-        stringBuilder.append(")");
-        if (root.right != null) {
-            stringBuilder.append("(");
-            populateString(root.right);
-            stringBuilder.append(")");
-        }
-    }
-
-
-    /*
-        Time complexity: O(n)
-        Space complexity: O(n)
-     */
+    final StringBuilder sb = new StringBuilder();
     public String tree2str(final TreeNode root) {
-        final StringBuilder sb = new StringBuilder();
+        populate(root);
+        return sb.toString();
+    }
+
+    void populate(final TreeNode root) {
         if (root == null) {
-            return sb.toString();
+            return;
         }
+
         sb.append(root.val);
         if (root.left != null) {
-            sb.append("(" + tree2str(root.left) + ")");
-        } else {
-            if (root.right != null) {
-                sb.append("()");
-            }
-        }
-        if (root.right != null) {
-            sb.append("(" + tree2str(root.right) + ")");
+            sb.append("(");
+            populate(root.left);
+            sb.append(")");
         }
 
-        return sb.toString();
+        if (root.right != null) {
+            if (root.left == null) {
+                sb.append("()");
+            }
+            sb.append("(");
+            populate(root.right);
+            sb.append(")");
+        }
     }
 
 }
--- learning/src/test/me/surendra/leetcode/trees/depth_first_search/pre_order_traversal/StringConstructorTest.java
+++ learning/src/test/me/surendra/leetcode/trees/depth_first_search/pre_order_traversal/StringConstructorTest.java
@@ -10,35 +10,17 @@ import static org.junit.Assert.assertThat;
 
 public class StringConstructorTest {
 
-    private String callMethod(final TreeNode treeNode) {
-        final StringConstructor stringConstructor = new StringConstructor();
+    private String callMethod(final Integer ...numbers) {
+        final TreeNode treeNode = BinaryTreeNodeCreator.fromPreOrder(numbers);
 
-        return stringConstructor.tree2str(treeNode);
-//        return stringConstructor.tree2strUsingHelper(treeNode);
+        return new StringConstructor().tree2str(treeNode);
     }
 
     @Test
-    public void test1() {
-        final TreeNode treeNode = BinaryTreeNodeCreator.fromPreOrder(1, 2, 3, 4);
-
-        assertThat(callMethod(treeNode), equalTo("1(2(4))(3)"));
-    }
-
-    @Test
-    public void test2() {
-        final TreeNode treeNode = BinaryTreeNodeCreator.fromPreOrder(1, 2, 3, null, 4);
-
-        assertThat(callMethod(treeNode), equalTo("1(2()(4))(3)"));
-    }
-
-    @Test
-    public void test3() {
-        final TreeNode treeNode = BinaryTreeNodeCreator.fromPreOrder(1, null, 2, 3, null);
-
-//        final TreeNode right = new TreeNode(2, new TreeNode(3), null);
-//        final TreeNode treeNode = new TreeNode(1, null, right);
-
-        assertThat(callMethod(treeNode), equalTo("1()(2(3))"));
+    public void test() {
+        assertThat(callMethod(1, 2, 3, 4), equalTo("1(2(4))(3)"));
+        assertThat(callMethod(1, 2, 3, null, 4), equalTo("1(2()(4))(3)"));
+        assertThat(callMethod(1, null, 2, 3, null), equalTo("1()(2(3))"));
     }
 
 }
