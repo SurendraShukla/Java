689d7ea77cfb8c4c485f1a689ffdda05824dd316
Surendra Shukla <surenshukla8@gmail.com>
2023-12-13 22:39:56 -0800

    LeetCode Binary Search - Find the Longest Valid Obstacle Course at Each Position

---
+++ learning/src/main/me/surendra/leetcode/search/binary_search/LongestValidObstacleCourseFinder.java
@@ -0,0 +1,54 @@
+package me.surendra.leetcode.search.binary_search;
+
+
+/**
+ * @see <a href="https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/">Find the Longest Valid Obstacle Course at Each Position</a>
+ */
+public class LongestValidObstacleCourseFinder {
+
+    /*
+        Time Complexity - O(n log n) Binary search (log n) for each element (n)
+        Space Complexity - O(1)
+     */
+    public int[] longestObstacleCourseAtEachPosition(final int[] obstacles) {
+        final int n = obstacles.length;
+        int lisLength = 0;
+
+        // lis[i] records the lowest increasing sequence of length i + 1.
+        final int[] answer = new int[n];
+        final int[] lis = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            final int height = obstacles[i];
+            // Find the rightmost insertion position idx.
+            final int idx = bisectRight(lis, height, lisLength);
+            if (idx == lisLength)
+                lisLength++;
+
+            lis[idx] = height;
+            answer[i] = idx + 1;
+        }
+        return answer;
+    }
+
+    // Find the rightmost insertion position. We use a fixed-length array and a changeable right boundary
+    // to represent an arraylist of dynamic size.
+    private int bisectRight(final int[] A, final int target, final int r) {
+        if (r == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = r;
+        int mid;
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (A[mid] <= target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+
+
+}
---
+++ learning/src/test/me/surendra/leetcode/search/binary_search/LongestValidObstacleCourseFinderTest.java
@@ -0,0 +1,41 @@
+package me.surendra.leetcode.search.binary_search;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+
+public class LongestValidObstacleCourseFinderTest {
+
+    private final LongestValidObstacleCourseFinder longestValidObstacleCourseFinder = new LongestValidObstacleCourseFinder();
+
+    private int[] callMethod(final int[] obstacles) {
+        return longestValidObstacleCourseFinder.longestObstacleCourseAtEachPosition(obstacles);
+    }
+
+    @Test
+    public void test() {
+        assertThat(
+            callMethod(new int[]{1, 2, 3, 2}),
+            equalTo(new int[]{1, 2, 3, 3})
+        );
+
+        assertThat(
+            callMethod(new int[]{2, 2, 1}),
+            equalTo(new int[]{1, 2, 1})
+        );
+
+        assertThat(
+            callMethod(new int[]{3, 1, 5, 6, 4, 2}),
+            equalTo(new int[]{1, 1, 2, 3, 2, 2})
+        );
+
+        assertThat(
+            callMethod(new int[]{5, 1, 5, 5, 1, 3, 4, 5, 1, 4}),
+            equalTo(new int[]{1, 1, 2, 3, 2, 3, 4, 5, 3, 5})
+        );
+    }
+
+}
